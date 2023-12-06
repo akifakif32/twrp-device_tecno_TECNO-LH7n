5039678f595945d4baa77b5440b4de8df4c9318d
akifakif32 <mhmtkfdrn@gmail.com>
2023-12-07 00:56:22 +0300

    Update twrp_LH7n.mk

--- twrp_LH7n.mk
+++ twrp_LH7n.mk
@@ -5,14 +5,14 @@
 #
 
 # Inherit from TECNO-LH7ndevice
-$(call inherit-product, device/tecno/TECNO-LH7n/device.mk)
+$(call inherit-product, device/tecno/LH7n/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Product Specifics
 PRODUCT_NAME := twrp_LH7n
-PRODUCT_DEVICE := TECNO-LH7n
+PRODUCT_DEVICE := LH7n
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO LH7n
 PRODUCT_MANUFACTURER := TECNO
